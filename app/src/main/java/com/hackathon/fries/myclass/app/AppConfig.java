9279a10fa6f9755776c9b63222a08874dbc8ce8e
Nguyen TienMinh <minhnt_58@vnu.edu.vn>
2015-11-20 21:09:01 +0700

    quan li database va controller (http)

---
+++ app/src/main/java/com/hackathon/fries/myclass/app/AppConfig.java
@@ -0,0 +1,9 @@
+package com.hackathon.fries.myclass.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://f.tutran.me/test/login";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://tutran.net/v1/register";
+}
