ebab403f6e6bf7333396a17555f778e0aab2a07e
quytm <quytm_58@vnu.edu.vn>
2015-11-21 00:35:10 +0700

    Thêm fragment hiển thị Thời khóa biểu cho sinh viên <commit lại>

---
+++ app/src/main/java/com/hackathon/fries/myclass/adapter/ItemLopMonHoc.java
@@ -0,0 +1,51 @@
+package com.hackathon.fries.myclass.adapter;
+
+/**
+ * Created by TMQ on 21-Nov-15.
+ */
+public class ItemLopMonHoc {
+    private String ten;
+    private String maLMH;
+    private String diaDiem;
+    private String giangVien;
+    private int viTri;
+    private int soTiet;
+    private int soSV;
+    private int nhomTH;
+
+    public ItemLopMonHoc(String ten, String maLMH, String diaDiem, String giangVien, int viTri, int soTiet, int soSV, int nhomTH){
+        this.ten        = ten;
+        this.maLMH      = maLMH;
+        this.diaDiem    = diaDiem;
+        this.giangVien  = giangVien;
+        this.viTri      = viTri;
+        this.soTiet     = soTiet;
+        this.soSV       = soSV;
+        this.nhomTH     = nhomTH;
+    }
+
+    public String getTen(){
+        return ten;
+    }
+    public String getMaLMH(){
+        return maLMH;
+    }
+    public String getDiaDiem(){
+        return diaDiem;
+    }
+    public String getGiangVien(){
+        return giangVien;
+    }
+    public int getViTri(){
+        return viTri;
+    }
+    public int getSoTiet(){
+        return soTiet;
+    }
+    public int getSoSV(){
+        return soSV;
+    }
+    public int getNhomTH(){
+        return nhomTH;
+    }
+}
---
+++ app/src/main/java/com/hackathon/fries/myclass/adapter/TableSubjectAdapter.java
@@ -0,0 +1,77 @@
+package com.hackathon.fries.myclass.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.hackathon.fries.myclass.R;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by TMQ on 20-Nov-15.
+ */
+public class TableSubjectAdapter extends BaseAdapter{
+    private ArrayList<ItemLopMonHoc> listSuject;
+    private ArrayList<ItemLopMonHoc> listSubjectInTable;
+    private Context mContext;
+    private LayoutInflater lf;
+
+    public TableSubjectAdapter(Context context){
+        mContext = context;
+        lf = LayoutInflater.from(mContext);
+        getSubjectData();
+    }
+
+    private void getSubjectData(){
+        listSuject = new ArrayList<>();
+
+//        listSuject.add(new ItemLopMonHoc("Xác suất thống kê", "MATH11111", "Giang vien",));
+//        listSuject.add(null);
+//        listSuject.add(new ItemLopMonHoc("Toán rời rạc", "MATH11111", "Giang vien", 100));
+//        listSuject.add(null);
+//        listSuject.add(null);
+//        listSuject.add(null);
+//        listSuject.add(new ItemLopMonHoc("Lý thuyết thông tin", "MATH11111", "Giang vien", 100));
+//        listSuject.add(null);
+//        listSuject.add(null);
+
+        listSubjectInTable.add(new ItemLopMonHoc("Xác suất thống kê", "MAT1111 1", "303 G2", "Lê Phê Đô", 3, 3, 90, 1));
+        listSubjectInTable.add(new ItemLopMonHoc("Toán rời rạc", "MAT2222 3", "313 G2", "Đặng Thanh Hải", 3, 3, 90, 1));
+    }
+
+    @Override
+    public int getCount() {
+        return listSuject.size();
+    }
+
+    @Override
+    public ItemLopMonHoc getItem(int position) {
+        return listSuject.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        if (view == null){
+            view = lf.inflate(R.layout.item_subject_in_table, null);
+        }
+
+        ItemLopMonHoc item = listSuject.get(position);
+        if (item == null) return view;
+
+        TextView txtName = (TextView)   view.findViewById(R.id.txtItemNameSubject);
+        txtName.setText(item.getTen());
+        txtName.setBackgroundColor(new Random().nextInt());
+
+        return view;
+    }
+}
