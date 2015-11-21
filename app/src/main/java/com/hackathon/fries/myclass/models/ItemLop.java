988ba8419da234330298900d68943c7ce4ca321d
Nguyen TienMinh <minhnt_58@vnu.edu.vn>
2015-11-21 07:56:43 +0700

    di chuyển file item sang package model

---
+++ app/src/main/java/com/hackathon/fries/myclass/models/ItemLop.java
@@ -0,0 +1,49 @@
+package com.hackathon.fries.myclass.models;
+
+/**
+ * Created by TooNies1810 on 11/20/15.
+ */
+public class ItemLop {
+    public String ten;
+    public String giangVien;
+    public int soNguoi;
+    public String id;
+
+    public String vietTat;
+
+    public ItemLop(String ten, String id, String giangVien, int soNguoi) {
+        this.ten = ten;
+        this.giangVien = giangVien;
+        this.soNguoi = soNguoi;
+        this.id = id;
+
+        locChuCaiDau();
+    }
+
+    void locChuCaiDau() {
+        if (ten == null) {
+            return;
+        }
+        vietTat = ten.charAt(0) + "";
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public String getGiangVien() {
+        return giangVien;
+    }
+
+    public int getSoNguoi() {
+        return soNguoi;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getVietTat() {
+        return vietTat;
+    }
+}
